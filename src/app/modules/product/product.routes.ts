901fd30de8428683549ccbba468ef0f2483de65f
jayed-phero <108389914+jayed-islam@users.noreply.github.com>
2024-12-04 22:45:28 +0600

    add product routes

---
+++ src/app/modules/product/product.routes.ts
@@ -0,0 +1,66 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { ProductController } from './product.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { ProductValidation } from './product.validation';
+import { USER_ROLE } from '../user/user.constants';
+import { multerUpload } from '../../config/multer.config';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import auth from '../../middlewares/auth';
+
+const router = Router();
+
+router.post(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.vendor),
+  multerUpload.array('files'),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.body && req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    } else {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        'Request body data is undefined or invalid JSON',
+      );
+    }
+
+    next();
+  },
+  validateRequest(ProductValidation.productSchema),
+  ProductController.createProduct,
+);
+
+router.post(
+  '/get-list',
+  validateRequest(ProductValidation.getProductListValidation),
+  ProductController.getProductList,
+);
+
+router.get('/get-single/:productId', ProductController.getSingleProductByID);
+
+router.get(
+  '/get-by-category/:categoryId',
+  ProductController.getProductByCategory,
+);
+
+router.post(
+  '/get-product-list',
+  auth(USER_ROLE.admin),
+  validateRequest(ProductValidation.getProductListValidation),
+  ProductController.getProductListForAdmin,
+);
+
+router.put(
+  '/:id',
+  auth(USER_ROLE.admin),
+  validateRequest(ProductValidation.updateProductValidationSchema),
+  ProductController.updateSingleProduct,
+);
+
+router.put(
+  '/delete/:id',
+  auth(USER_ROLE.admin),
+  ProductController.softDeleteProduct,
+);
+
+export const ProductRoutes = router;
